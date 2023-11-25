50e2f5da973fc0880680da1221ad4f6051134e24
Bera Yılmaz <142394009+BeraYilmaZzz@users.noreply.github.com>
2023-11-25 18:07:21 +0300

    StudentGUI.java

--- com/patikadev/View/StudentGUI.java
+++ com/patikadev/View/StudentGUI.java
@@ -1,282 +1,138 @@
-package com.patikadev.View;
+package patika_clone.View;
 
-import com.patikadev.Helper.Config;
-import com.patikadev.Helper.Helper;
-import com.patikadev.Helper.Item;
-import com.patikadev.Model.*;
-
+import patika_clone.Helper.Config;
+import patika_clone.Helper.Helper;
+import patika_clone.Model.Course;
+import patika_clone.Model.Patika;
+import patika_clone.Model.Student;
+import patika_clone.Model.StudentCourse;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class StudentGUI extends JFrame{
+public class StudenGUI extends JFrame {
     private JPanel wrapper;
-    private JTabbedPane tab_student;
-    private JButton logoutButton;
-    private JScrollPane scrl_student_patika_register;
-    private JTable tbl_student_course_register;
-    private JLabel lbl_welcome;
-    private JComboBox cmb_patika_list;
-    private JComboBox cmb_course_list;
-    private JButton btn_register_course;
-    private JTextField fld_course_delete;
-    private JButton btn_course_delete;
-    private JPanel pnl_course_contents;
-    private JScrollPane scrl_course_contents;
-    private JTable tbl_student_course_contents;
-    private JTextField fld_course_contents_remove;
-    private JButton btn_course_contents_delete;
-    private JTextField fld_srch_course_contents_course_name;
-    private JTextField fld_srch_course_contents_contents_title;
-    private JButton btn_srch_course_contents;
-    private JComboBox cmb_contents_course_name;
-    private JComboBox cmb_contents_title;
-    private JButton btn_course_contents_add;
-    private JComboBox cmb_contents_quiz_question;
-    private JTextField fld_contents_quiz_answer;
-    private JTextField fld_contents_comment;
-    DefaultTableModel mdl_student_course_register_list;
-    Object[] row_student_course_register_list;
-    DefaultTableModel mdl_student_course_contents_list;
-    Object[] row_student_course_contents_list;
+    private JTabbedPane pane_student;
+    private JPanel panel_patika;
+    private JScrollPane scroll_patika;
+    private JTable table_stu_patika;
+    private JPanel panel_your_patika;
+    private JScrollPane scroll_your_patika;
+    private JTable table_own_patika;
+    private JTextField field_choose_patika;
+    private JButton addButton;
+    private JTable table_own_course;
+    private JButton exitButton;
+    private DefaultTableModel model_stu_patika;
+    private Object[] row_stu_patika;
+    private int id;
+    private DefaultTableModel model_own_patika;
+    private Object[] row_own_patika;
+    private DefaultTableModel model_own_courses;
+    private Object[] row_own_courses;
 
-    private Student student;
-
-    public StudentGUI(Student student){
-        this.student = student;
+    public StudenGUI(int user_id) {
         add(wrapper);
-        setSize(1000,650);
-        setLocation(Helper.screenCenterPoint("x",getSize()) , Helper.screenCenterPoint("y" , getSize()));
+        setSize(500, 500);
+        setLocation(Helper.screenCenter("x", getSize()), Helper.screenCenter("y", getSize()));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle(Config.PROJECT_TITLE);
-        setResizable(true);
         setVisible(true);
+        setResizable(false);
 
-        lbl_welcome.setText("Welcome" + student.getName());
-        mdl_student_course_register_list = new DefaultTableModel(){
-            @Override
-            public boolean isCellEditable(int row, int column) {
-                if ((column == 0) || (column == 1))
-                    return false;
+          model_own_courses = new DefaultTableModel();
+        Object[] col_own_courses = {"ID", "Patika ID","Course Name","Patika Name"};
+        model_own_courses.setColumnIdentifiers(col_own_courses);
+        row_own_courses = new Object[col_own_courses.length];
+        table_own_course.setModel(model_own_courses);
+        table_own_course.getTableHeader().setReorderingAllowed(false);
+        table_own_course.getColumnModel().getColumn(0).setMaxWidth(60);
+        loadOwnCourses();
+        
+        model_stu_patika = new DefaultTableModel();
+        Object[] col_stu_patika = {"ID", "Patika"};
+        model_stu_patika.setColumnIdentifiers(col_stu_patika);
+        row_stu_patika = new Object[col_stu_patika.length];
+        table_stu_patika.setModel(model_stu_patika);
+        table_stu_patika.getTableHeader().setReorderingAllowed(false);
+        table_stu_patika.getColumnModel().getColumn(0).setMaxWidth(60);
+        loadPatika();
 
-                return super.isCellEditable(row, column);
-            }
-        };
-        Object[] col_student_course_register_list = {"ID" , "Patika Name" , "Course Name" , "Educator Name"};
-        mdl_student_course_register_list.setColumnIdentifiers(col_student_course_register_list);
-        row_student_course_register_list = new Object[col_student_course_register_list.length];
-        loadCourseRegisterModel();
-        loadPatikasCmbBox();
-        tbl_student_course_register.setModel(mdl_student_course_register_list);
-        tbl_student_course_register.getColumnModel().getColumn(0).setMaxWidth(50);
-        tbl_student_course_register.getColumnModel().getColumn(0).setMinWidth(25);
-        tbl_student_course_register.getTableHeader().setReorderingAllowed(false);
+        model_own_patika = new DefaultTableModel();
+        Object[] col_own_patika = {"ID", "Patika"};
+        model_own_patika.setColumnIdentifiers(col_own_patika);
+        row_own_patika = new Object[col_own_patika.length];
+        table_own_patika.setModel(model_own_patika);
+        table_own_patika.getTableHeader().setReorderingAllowed(false);
+        table_own_patika.getColumnModel().getColumn(0).setMaxWidth(60);
+        loadOwnPatika();
 
-        tbl_student_course_register.getSelectionModel().addListSelectionListener(e -> {
-            try {
-                String selected_row = tbl_student_course_register.getValueAt(tbl_student_course_register.getSelectedRow() , 0).toString();
-                fld_course_delete.setText(selected_row);
-            }catch (Exception ex){
-            }
-        });
-        //CourseRegisterModel---
-
-        //CourseContentsModel
-        mdl_student_course_contents_list = new DefaultTableModel(){
-            @Override
-            public boolean isCellEditable(int row, int column) {
-                if ((column == 0) || (column == 1))
-                    return false;
-
-                return super.isCellEditable(row, column);
-            }
-        };
-        Object[] col_student_course_contents_list = {"ID"  , "Course Name" , "Contents Title" ,
-                "Contents Questions" , "Student Answer" , "Student Comment"};
-        mdl_student_course_contents_list.setColumnIdentifiers(col_student_course_contents_list);
-        row_student_course_contents_list = new Object[col_student_course_contents_list.length];
-        loadStudentAnswerModel();
-        loadContentsCourseCmbBox();
-        tbl_student_course_contents.setModel(mdl_student_course_contents_list);
-        tbl_student_course_contents.getColumnModel().getColumn(0).setMaxWidth(50);
-        tbl_student_course_contents.getColumnModel().getColumn(0).setMinWidth(25);
-        tbl_student_course_contents.getTableHeader().setReorderingAllowed(false);
-
-        tbl_student_course_contents.getSelectionModel().addListSelectionListener(e -> {
-            try {
-                String selected_row = tbl_student_course_contents.getValueAt(tbl_student_course_contents.getSelectedRow() , 0).toString();
-                fld_course_contents_remove.setText(selected_row);
-            }catch (Exception ex){
-            }
-        });
-        //CourseContentsModel---
-
-
-
-        btn_register_course.addActionListener(e -> {
-            Item patikaItem = (Item) cmb_patika_list.getSelectedItem();
-            Item courseItem = (Item) cmb_course_list.getSelectedItem();
-            int patikaID = patikaItem.getKey();
-            int courseID = courseItem.getKey();
-
-            if (StudentCourse.add(patikaID , courseID)){
-                Helper.showMsg("done");
-                loadCourseRegisterModel();
-                loadContentsCourseCmbBox();
-                loadQuizQuestionCmbBox();
-                loadContentsTitleCmbBox();
-            }else {
-                Helper.showMsg("error");
-            }
-
-        });
-
-        cmb_patika_list.addActionListener(e -> {
-            loadCoursesCmbBox();
-            cmb_course_list.setSelectedItem(0);
-        });
-
-        logoutButton.addActionListener(e -> {
-            dispose();
-            LoginGUI loginGUI = new LoginGUI();
-        });
-        btn_course_delete.addActionListener(e -> {
-            int selected_course = Integer.parseInt(fld_course_delete.getText());
-            if ( Helper.isFieldEmpty(fld_course_delete)){
+        addButton.addActionListener(e -> {
+            if (field_choose_patika.getText().isEmpty()){
                 Helper.showMsg("fill");
             }else {
-                StudentCourse.delete(selected_course);
-                Helper.showMsg("done");
-                StudentCourseContents.deleteByCourseID(selected_course);
-                loadCourseRegisterModel();
-                loadStudentAnswerModel();
-                loadContentsCourseCmbBox();
-                loadQuizQuestionCmbBox();
-                loadContentsTitleCmbBox();
-            }
-        });
-
-        cmb_contents_course_name.addActionListener(e -> {
-            loadContentsTitleCmbBox();
-            cmb_contents_title.addActionListener(ex ->{
-                loadQuizQuestionCmbBox();
-
-            });
-
-        });
-
-        btn_course_contents_add.addActionListener(e -> {
-            Item courseItem = (Item) cmb_contents_course_name.getSelectedItem();
-            Item contentsItem = (Item) cmb_contents_title.getSelectedItem();
-            Item quizItem = (Item) cmb_contents_quiz_question.getSelectedItem();
-            int courseID = courseItem.getKey();
-            int contentsID = contentsItem.getKey();
-            int quizID = quizItem.getKey();
-            String answer = fld_contents_quiz_answer.getText();
-            String comment = fld_contents_comment.getText();
-
-            if (Helper.isFieldEmpty(fld_contents_quiz_answer) && Helper.isFieldEmpty(fld_contents_comment) &&
-                    Helper.isFieldEmpty(cmb_contents_course_name) && Helper.isFieldEmpty(cmb_contents_title) &&
-                    Helper.isFieldEmpty(cmb_contents_quiz_question)){
-                Helper.showMsg("fiil");
-            }else {
-                if (StudentCourseContents.add(courseID , contentsID , quizID , answer , comment)){
+                int ID = Integer.parseInt(field_choose_patika.getText());
+                String name = String.valueOf(Patika.getFetch(ID));
+                if (Student.add(ID,name)){
                     Helper.showMsg("done");
-                    loadStudentAnswerModel();
-                }else {
-                    Helper.showMsg("error");
+                    loadOwnPatika();
+                    field_choose_patika.setText(null);
                 }
-            }
-            fld_contents_comment.setText(null);
-            fld_contents_quiz_answer.setText(null);
-        });
-
-        btn_course_contents_delete.addActionListener(e -> {
-            int selected_contents = Integer.parseInt(fld_course_contents_remove.getText());
-            if (fld_course_contents_remove != null  && Helper.isFieldEmpty(fld_course_contents_remove)){
-                Helper.showMsg("fiil");
-            }else {
-                StudentCourseContents.delete(selected_contents);
-                Helper.showMsg("done");
-                loadStudentAnswerModel();
-                loadContentsCourseCmbBox();
-                loadQuizQuestionCmbBox();
-                loadContentsTitleCmbBox();
+                StudentCourse.addCourse(StudentCourse.getFetch(ID));
+                loadOwnCourses();
             }
         });
-
+        
+        exitButton.addActionListener(e -> {
+            dispose();
+            LoginGUI login = new LoginGUI();
+        });
+        
     }
 
-    private void loadCourseRegisterModel() {
-        DefaultTableModel clearModel = (DefaultTableModel) tbl_student_course_register.getModel();
+    private void loadOwnCourses() {
+        DefaultTableModel clearModel = (DefaultTableModel) table_own_course.getModel();
         clearModel.setRowCount(0);
-        int i;
-        for (StudentCourse obj : StudentCourse.getList()){
-            i = 0;
-            row_student_course_register_list[i++] = obj.getId();
-            row_student_course_register_list[i++] = Patika.getFetch(obj.getPatika_id()).getName();
-            row_student_course_register_list[i++] = Course.getFetch(obj.getCourse_id()).getName();
-            row_student_course_register_list[i++] = User.getFetch(Course.getFetch(obj.getCourse_id()).getUser_id()).getName();
-            mdl_student_course_register_list.addRow(row_student_course_register_list);
+        ArrayList<Course> courses = StudentCourse.getListByCourse();
+        for (Course path : courses) {
+            int i = 0;
+            row_own_courses[i++] = path.getId();
+            row_own_courses[i++] = path.getPatika_id();
+            row_own_courses[i++] = path.getName();
+            row_own_courses[i++] = path.getLanguage();
+            model_own_courses.addRow(row_own_courses);
         }
     }
 
-    private void loadStudentAnswerModel() {
-        DefaultTableModel clearModel = (DefaultTableModel) tbl_student_course_contents.getModel();
+    public void loadOwnPatika() {
+        DefaultTableModel clearModel = (DefaultTableModel) table_own_patika.getModel();
         clearModel.setRowCount(0);
-        int i;
-        for (StudentCourseContents obj : StudentCourseContents.getList()){
-            i = 0;
-            row_student_course_contents_list[i++] = obj.getId();
-            row_student_course_contents_list[i++] = Course.getFetch(obj.getCourse_id()).getName();
-            row_student_course_contents_list[i++] = Contents.getFetchByCourseID(obj.getCourse_id()).getTitle();
-            row_student_course_contents_list[i++] = Quiz.getFetch(obj.getQuiz_id()).getQuiz_question();
-            row_student_course_contents_list[i++] =obj.getAnswer();
-            row_student_course_contents_list[i++] =obj.getComment();
-            mdl_student_course_contents_list.addRow(row_student_course_contents_list);
+        ArrayList<Patika> patika = Student.getListByPatika();
+
+        for (Patika path : patika) {
+            int i = 0;
+            row_own_patika[i++] = path.getId();
+            row_own_patika[i++] = path.getName();
+            model_own_patika.addRow(row_own_patika);
         }
     }
 
-    public void loadPatikasCmbBox(){
-        cmb_patika_list.removeAllItems();
-        cmb_patika_list.addItem(new Item(0,null));
-        for ( Patika obj : Patika.getList()){
-            cmb_patika_list.addItem(new Item(obj.getId(), obj.getName()));
+    public void loadPatika() {
+        DefaultTableModel clearModel = (DefaultTableModel) table_stu_patika.getModel();
+        clearModel.setRowCount(0);
+        ArrayList<Patika> patika = Patika.getListByUser();
+        if (patika.isEmpty()) {
+            Helper.showMsg("There is no path to choose :( Please contact with Patika to ask adding new courses :(");
+        } else {
+            for (Patika path : patika) {
+                int i = 0;
+                row_stu_patika[i++] = path.getId();
+                row_stu_patika[i++] = path.getName();
+                model_stu_patika.addRow(row_stu_patika);
+            }
         }
     }
-    public void loadCoursesCmbBox(){
-        Item patikaItem = (Item) cmb_patika_list.getSelectedItem();
-        cmb_course_list.removeAllItems();
-        cmb_course_list.addItem(new Item(0,null));
-        for ( Course obj : Course.getListByPatikaID(patikaItem.getKey())){
-            cmb_course_list.addItem(new Item(obj.getId(), obj.getName()));
-        }
-    }
-
-    public void loadContentsCourseCmbBox(){
-        cmb_contents_course_name.removeAllItems();
-        cmb_contents_course_name.addItem(new Item(0,null));
-        for ( StudentCourse obj : StudentCourse.getList()){
-            cmb_contents_course_name.addItem(new Item(obj.getId() , Course.getFetch(obj.getCourse_id()).getName()));
-        }
-    }
-
-    public void loadContentsTitleCmbBox(){
-        Item courseItem = (Item) cmb_contents_course_name.getSelectedItem();
-        cmb_contents_title.removeAllItems();
-        cmb_contents_title.addItem(new Item(0,null));
-        for (Contents obj : Contents.getListByCourseID(courseItem.getKey())){
-            cmb_contents_title.addItem(new Item(obj.getId(), obj.getTitle()));
-        }
-    }
-
-    public void loadQuizQuestionCmbBox(){
-        Item contentsItem = (Item) cmb_contents_title.getSelectedItem();
-        cmb_contents_quiz_question.removeAllItems();
-        cmb_contents_quiz_question.addItem(new Item(0,null));
-
-        for (Quiz obj : Quiz.getListByContents(contentsItem.getKey())){
-            cmb_contents_quiz_question.addItem(new Item(obj.getId(), obj.getQuiz_question()));
-        }
-
-    }
+    
 }
